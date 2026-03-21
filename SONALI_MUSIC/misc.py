9f3238905aff427117eac6f252c3d11369f66dbf
ᡣ𐭩 ེ𝚱𝛖ɭꝛ᪳ᷱ𝛖𝐯𝛊𝁜ֶָ֪֯ 𝁘💋 <kuruvinetwork@gmail.com>
2026-03-21 18:56:12 +0530

    Update misc.py

--- SONALI_MUSIC/misc.py
+++ SONALI_MUSIC/misc.py
@@ -87,5 +87,5 @@ def heroku():
 
 # 🧑‍💻 Developer : t.me/itzmekuruvi
 # 🔗 Source link : GitHub.com/Im-Notcoder/Sonali-MusicV2
-# 📢 Telegram channel : t.me/Purvi_Bots
+# 📢 Telegram channel : t.me/kuruvibotz
 # =======================================================
