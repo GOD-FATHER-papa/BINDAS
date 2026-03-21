d3ab021006a13ae98341b392dbbde344caf22efa
ᡣ𐭩 ེ𝚱𝛖ɭꝛ᪳ᷱ𝛖𝐯𝛊𝁜ֶָ֪֯ 𝁘💋 <kuruvinetwork@gmail.com>
2026-03-21 18:57:53 +0530

    Update __main__.py

--- SONALI_MUSIC/__main__.py
+++ SONALI_MUSIC/__main__.py
@@ -74,5 +74,5 @@ if __name__ == "__main__":
 
 # 🧑‍💻 Developer : t.me/itzmekuruvi
 # 🔗 Source link : GitHub.com/Im-Notcoder/Sonali-MusicV2
-# 📢 Telegram channel : t.me/Purvi_Bots
+# 📢 Telegram channel : t.me/kuruvibotz
 # =======================================================
