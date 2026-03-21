c92a347429ea82ef2281523cb01191236b35e53b
ᡣ𐭩 ེ𝚱𝛖ɭꝛ᪳ᷱ𝛖𝐯𝛊𝁜ֶָ֪֯ 𝁘💋 <kuruvinetwork@gmail.com>
2026-03-21 18:57:01 +0530

    Update logging.py

--- SONALI_MUSIC/logging.py
+++ SONALI_MUSIC/logging.py
@@ -31,5 +31,5 @@ def LOGGER(name: str) -> logging.Logger:
 
 # 🧑‍💻 Developer : t.me/itzmekuruvi
 # 🔗 Source link : GitHub.com/Im-Notcoder/Sonali-MusicV2
-# 📢 Telegram channel : t.me/Purvi_Bots
+# 📢 Telegram channel : t.me/kuruvibotz
 # =======================================================
