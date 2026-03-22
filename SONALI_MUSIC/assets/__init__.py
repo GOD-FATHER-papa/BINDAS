d3db33c5e31dfeba7bb462cae7410a7ff37e99bd
ᡣ𐭩 ེ𝚱𝛖ɭꝛ᪳ᷱ𝛖𝐯𝛊𝁜ֶָ֪֯ 𝁘💋 <kuruvinetwork@gmail.com>
2026-03-22 13:55:20 +0530

    Update __init__.py

--- SONALI_MUSIC/assets/__init__.py
+++ SONALI_MUSIC/assets/__init__.py
@@ -1,17 +1,17 @@
 # =======================================================
-# ©️ 2025-26 All Rights Reserved by Purvi Bots (Im-Notcoder) 🚀
+# ©️ 2025-26 All Rights Reserved by kuruvi Bots (Im-Notcoder) 🚀
 
 # This source code is under MIT License 📜 Unauthorized forking, importing, or using this code without giving proper credit will result in legal action ⚠️
  
-# 📩 DM for permission : @TheSigmaCoder
+# 📩 DM for permission : @KURUVI_TELE
 # =======================================================
 
 
 
 # ======================================================
-# ©️ 2025-26 All Rights Reserved by Purvi Bots (Im-Notcoder) 😎
+# ©️ 2025-26 All Rights Reserved by kuruvi Bots (Im-Notcoder) 😎
 
-# 🧑‍💻 Developer : t.me/TheSigmaCoder
+# 🧑‍💻 Developer : t.me/itzmekuruvi
 # 🔗 Source link : GitHub.com/Im-Notcoder/Sonali-MusicV2
-# 📢 Telegram channel : t.me/Purvi_Bots
+# 📢 Telegram channel : t.me/kuruvibotz
 # =======================================================
