7a8117fcda9eaa7adc4bcc73950a30589e616dfd
ᡣ𐭩 ེ𝚱𝛖ɭꝛ᪳ᷱ𝛖𝐯𝛊𝁜ֶָ֪֯ 𝁘💋 <kuruvinetwork@gmail.com>
2026-03-24 11:16:36 +0530

    Update gitinfo.py

--- SONALI_MUSIC/plugins/tools/gitinfo.py
+++ SONALI_MUSIC/plugins/tools/gitinfo.py
@@ -20,7 +20,7 @@ executor = ThreadPoolExecutor(max_workers=3)
 @app.on_message(filters.command(["github", "git"]))
 async def github(_, message):
     if len(message.command) != 2:
-        await message.reply_text("`/git TEAM-ISHA`")
+        await message.reply_text("`/git TEAM-KURUVI`")
         return
 
     username = message.text.split(None, 1)[1]
