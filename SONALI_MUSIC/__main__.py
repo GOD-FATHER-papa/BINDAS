fec1a7b5830923b41aea17bd03748da561e77bdc
ᡣ𐭩 ེ𝚱𝛖ɭꝛ᪳ᷱ𝛖𝐯𝛊𝁜ֶָ֪֯ 𝁘💋 <kuruvinetwork@gmail.com>
2026-04-03 09:25:34 +0530

    Update __main__.py

--- SONALI_MUSIC/__main__.py
+++ SONALI_MUSIC/__main__.py
@@ -48,7 +48,7 @@ async def init():
     await userbot.start()
     await Sona.start()
     try:
-        await Sona.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await Sona.stream_call("https://files.catbox.moe/zexvqh.mp4")
     except NoActiveGroupCall:
         LOGGER("SONALI_MUSIC").error(
             "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧/𝗖𝗛𝗔𝗡𝗡𝗘𝗟... 😒\n\n𝗠𝗨𝗦𝗜𝗖 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........🤕"
