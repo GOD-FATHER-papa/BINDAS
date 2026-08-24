26f42fe5427609dd739faa03e71310f41c288815
⎯᪵⎯꯭̽🚬᪵᪳ ⃪꯭𝐁ʌ‌꯭ᴅ꯭𝐍꯭ᴀ‌꯭ᴍ꯭ ꯭🖤᪵᪳᪳𝆺꯭𝅥⎯꯭̽⟶᯦꯭ <rejemtahir@gmail.com>
2026-08-24 09:36:00 +0530

    Replace youtubesearchpython with py_yt for video search

--- SONALI_MUSIC/utils/thumbnails.py
+++ SONALI_MUSIC/utils/thumbnails.py
@@ -12,7 +12,7 @@ import random
 import aiofiles
 import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
-from youtubesearchpython.__future__ import VideosSearch
+from py_yt import VideosSearch, Playlist
 from config import YOUTUBE_IMG_URL
 from SONALI_MUSIC import app
 
